b8e1d3df031d03519502fecdbb08e254c954a62b
cindy <cindy.nicolay@laposte.net>
2017-07-12 16:37:32 +0200

    table with different color of cards in main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
 
+
+var colorsArray = ["Violet", "DeepPink","GhostWhite","Olive", "Aqua","cadetBlue", "coral", "Violet", "DeepPink","GhostWhite","Olive", "Aqua","cadetBlue", "coral",];
+
+var randomColor= ["","","","","","","","","","","","","",""];
+
+
+var changeColor= document.getElementsByClassName("poussinColor");
+var currentColor;
+
+for (let i=0; i<changeColor.length; i++){
+  changeColor[i].onclick=function(){
+    this.style.backgroundColor =colorsArray[i];
+  }
+}
