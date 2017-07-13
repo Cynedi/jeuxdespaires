1a01b8876e9e0ab25e7afa65dfd56090aaaec005
cindy <cindy.nicolay@laposte.net>
2017-07-13 11:13:14 +0200

    random on function onclick

--- js/main.js
+++ js/main.js
@@ -2,14 +2,35 @@
 
 var colorsArray = ["Violet", "DeepPink","GhostWhite","Olive", "Aqua","cadetBlue", "coral", "Violet", "DeepPink","GhostWhite","Olive", "Aqua","cadetBlue", "coral",];
 
-var randomColor= ["","","","","","","","","","","","","",""];
+var randomColor= [""];
+
+// var cards=document.getElementsByClassName("poussinColor");
+// console.log (cards);
+//
+//
+//
+// for (var i=0; i<colorsArray.length; i++) {
+//   var triage = colorsArray[Math.floor(Math.random()*colorsArray.length)];
+//   cards[i].backgroundColor=triage[i];
+// }
+//
+//
+
+
+
+
+
 
 
 var changeColor= document.getElementsByClassName("poussinColor");
 var currentColor;
 
 for (let i=0; i<changeColor.length; i++){
+
   changeColor[i].onclick=function(){
-    this.style.backgroundColor =colorsArray[i];
+      var triage = colorsArray[Math.floor(Math.random()*colorsArray.length)];
+      this.style.backgroundColor =triage;
+      console.log (this);
+      console.log (changeColor[i]);
   }
 }
