8abec1ad0b231fb469b09d508f34952ea511419b
cindy <cindy.nicolay@laposte.net>
2017-07-13 11:22:34 +0200

    made  new array for push colors

--- js/main.js
+++ js/main.js
@@ -2,35 +2,21 @@
 
 var colorsArray = ["Violet", "DeepPink","GhostWhite","Olive", "Aqua","cadetBlue", "coral", "Violet", "DeepPink","GhostWhite","Olive", "Aqua","cadetBlue", "coral",];
 
-var randomColor= [""];
-
-// var cards=document.getElementsByClassName("poussinColor");
-// console.log (cards);
-//
-//
-//
-// for (var i=0; i<colorsArray.length; i++) {
-//   var triage = colorsArray[Math.floor(Math.random()*colorsArray.length)];
-//   cards[i].backgroundColor=triage[i];
-// }
-//
-//
-
-
-
-
-
 
 
 var changeColor= document.getElementsByClassName("poussinColor");
-var currentColor;
+
+var newArray=[];
+
 
 for (let i=0; i<changeColor.length; i++){
 
   changeColor[i].onclick=function(){
       var triage = colorsArray[Math.floor(Math.random()*colorsArray.length)];
       this.style.backgroundColor =triage;
-      console.log (this);
-      console.log (changeColor[i]);
+      newArray.push(triage);
+      console.log (newArray);
   }
+
+
 }
