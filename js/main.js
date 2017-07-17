17886c86530109523538858c7fa90461810e2386
cindy <cindy.nicolay@laposte.net>
2017-07-17 21:22:32 +0200

    add a set time out

--- js/main.js
+++ js/main.js
@@ -19,7 +19,7 @@ for (let i=0; i<changeColor.length; i++){
 
 changeColor[i].style.backgroundColor = triage[i];
 
-article.onclick=function(){
+document.getElementsByClassName('poussinColor').onclick=function(){
 
 //récupération des couleurs cliquées dans un nouveau tableau//
 newArray.push(triage[i]);
@@ -29,16 +29,17 @@ if (newArray.length===2) {//arriver a 2 cliques//
 //comparaison entre la première couleur et la 2eme sur lequel il y a un click//
 
     if (newArray[0]!== newArray[1]) {
-    article.style.display='block';//la couleur jaune poussin reste//
+    document.getElementsByClassName('poussinColor').style.display='block';//la couleur jaune poussin reste//
+    } else {
+    document.getElementsByClassName('poussinColor').style.display='none';//la couleur jaune poussin disparait//
+    article.parentNode.style.backgroundColor=newArray[i];// elle est remplacée par la couleur récupérée dans le nouveau tableau//
 
-    else {
-      article.style.display="none";//la couleur jaune poussin disparait//
-      parentNode.style.backgroundColor=newArray[i];
+  var x=setTimeout(2000); //se met en display none au bout de 2 sec//
 
     var newArray=[]; //on vide le tableau pour qu'il y ai tjs 2 comparaison//
     }
 
-      }
+
 
 
   }
